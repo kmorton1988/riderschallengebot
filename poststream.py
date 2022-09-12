e96844e59c7c7483ea82a0f68524d2041f6a92df
Kyle Morton <kyle@gomorton.com>
2022-09-12 17:36:00 +0000

    removing poststream.py from root

--- poststream.py
+++
@@ -1,4 +0,0 @@
-# listening to stream for subreddit
-def poststream():
-    for submissions in reddit.subreddit("RidersChallengeTest").stream.submissions(skip_existing=True):
-        iscomplete(submissions)
