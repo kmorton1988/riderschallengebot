03799b9311210b31d0a767a31778ef40e3fd90e9
Kyle Morton <kyle@gomorton.com>
2022-09-13 03:04:30 +0000

    refactoring for cleaner code and less unneeded abstraction

--- functions/db.py
+++ functions/db.py
@@ -2,28 +2,25 @@ import sqlite3
 from sqlite3 import Error
 
 # Creates database connection, creates 'users' table if there is no existing 'users' table
-def create_connection(db_file):
-    con = None
-    try:
-        con = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-    if con:
-        cur = con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS users (user STRING PRIMARY KEY UNIQUE, points INTEGER, flair STRING);")
-        con.commit
-    return con
-
-def get_points(file,user):
-    # Get's current Points by Connecting to the file and executing a command to return current value in "points". returns "None" even if you attempt to convert "None" to 0 explicitly. 
-    con = create_connection(file)
+db_file = "../RCMaster.db"
+con = None
+try:
+    con = sqlite3.connect(db_file)
+except Error as e:
+    print(e)
+if con:
     cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (user STRING PRIMARY KEY UNIQUE, points INTEGER, flair STRING);")
+    con.commit()
+
+
+def get_points(user):
+    # Get's current Points by Connecting to the file and executing a command to return current value in "points". returns "None" even if you attempt to convert "None" to 0 explicitly. 
     for row in cur.execute("""SELECT points from users where user = ?;""",(user,)):
         return int(row[0])
 
-def get_flair(file,user):
+def get_flair(user):
     # get's current flair and passes the value forward. mod_flair() has logic to handle None value. (there is no other reason to have this method called from anywhere else)
-    con = create_connection(file)
     cur = con.cursor()
     for row in cur.execute("""select flair from users where user = ?""",(user,)):
         if row[0] == None:
@@ -31,52 +28,56 @@ def get_flair(file,user):
         else:
             return row[0]
 
+def user_exists(user):
+    if cur.execute("select 1 from users where user = ?;", (user, )).fetchone():
+        return True
 
 # Function for adding a point for a completed post
-def add_point(file,user):
-    # Creates database connection
-    con = create_connection(file)
+# def add_point(file,user):
+#     # Creates database connection
+#     con = create_connection(file)
     
-    # and the database cursor
-    cur = con.cursor()
+#     # and the database cursor
+#     cur = con.cursor()
     
-    # sets a flair variable for later use
-    flair = get_flair(file,user)
+#     # sets a flair variable for later use
+#     flair = get_flair(file,user)
 
-    # sets the current points to a variable
-    current_points = get_points(file,user)
-    if current_points == None:
-        current_points = 0    
-    # Adds a point. 
-    new_points = current_points + 1
+#     # sets the current points to a variable
+#     current_points = get_points(file,user)
+#     if current_points == None:
+#         current_points = 0    
+#     # Adds a point. 
+#     new_points = current_points + 1
 
-    # Modifies the values of the existing user, or creates a new row in case the user does not yet exist. It's okay for flair to be entered as None if no flair exists for the user.
-    cur.execute("""INSERT OR REPLACE INTO users (user,points,flair) VALUES (?,?,?);""",(user, new_points, flair,))
+#     # Modifies the values of the existing user, or creates a new row in case the user does not yet exist. It's okay for flair to be entered as None if no flair exists for the user.
+#     cur.execute("""INSERT OR REPLACE INTO users (user,points,flair) VALUES (?,?,?);""",(user, new_points, flair,))
     
-    # commites the change to the database
-    con.commit()
+#     # commites the change to the database
+#     con.commit()
     
-    # closes the connection to the database. 
-    con.close()
+#     # closes the connection to the database. 
+#     con.close()
 
-def mod_flair(file,user,flair):
-    # Takes 3 arguments: File: database file, User: user to be modified, Flair: custom flair requested
+def add_point(user):
+    if user_exists(user):
+        cur.execute("UPDATE users SET points = ? WHERE user = ?;", (get_points(user) + 1,
+                                                                        user))
+    else:
+        cur.execute("INSERT INTO users (user, points) VALUES (?, ?);", (user, 0))
 
-    # Database Connection Created
-    con = create_connection(file)
-    
-    # Database Cursor created
-    cur = con.cursor()
+def mod_flair(user,flair):
+    # Takes 2 arguments:  User: user to be modified, Flair: custom flair requested
 
-    # Capture current points
-    current_points = get_points(file,user)
+
+    current_points = get_points(user)
 
     # Ensuring an integer is eventually passed, in case of no points for user
     if current_points == None:
         current_points = 0
     
     #capture current flair
-    current_flair = get_flair(file,user)
+    current_flair = get_flair(user)
 
     #Checks for existing flair. If flair is found, it overwrites existing flair with new flair. If no flair is found, it creates the flair. 
     if current_flair == None:
--- run.py
+++ run.py
@@ -1,21 +1,43 @@
 import functions.reddit as reddit
-
+import praw
+import functions.db as db
 
 KEYWORDS = ["complete"]
 
 #Establish a Reddit Session
-session = reddit.connect()
+con = praw.Reddit()
+
+def process_post(post):
+    user = post.author
+    db.add_point(user)
+    flair_update = db.get_flair(user)
+    return flair_update
+
+def generate_flair_text(user, existing_flair):
+    try:
+        user_flair_text, _ = existing_flair.split(" | ", 1)
+    except ValueError:
+        user_flair_text = existing_flair
+
+    points = db.get_points(user) or 0
+
+    if user_flair_text:
+        return f"{user_flair_text} | {points}"
+    else:
+        return str(points)
+
+
 
 # Listening for Complete keyword in titles to reply to. 
-for post in session.subreddit("riderschallengetest").stream.submissions():
+for post in reddit.subreddit("riderschallengetest").stream.submissions():
     if post.saved:
         continue
     has_keyword = any(k.lower() in post.title.lower() for k in KEYWORDS)
-    not_self = post.author != session.user.me()
+    not_self = post.author != con.user.me()
     if has_keyword and not_self:
         post.save()
         to_post = reddit.process_post(post)
-        session.subreddit("riderschallengetest").flair.set(post.author, text=to_post)
+        con.subreddit("riderschallengetest").flair.set(post.author, text=to_post)
 
 
     
