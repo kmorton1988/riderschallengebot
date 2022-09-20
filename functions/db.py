d072178f4e6bc5560c084e8aa3cb91948f4c0971
Kyle Morton <kyle@gomorton.com>
2022-09-20 05:47:35 +0000

    First attempt at incorporating multi-stream

--- functions/db.py
+++ functions/db.py
@@ -2,50 +2,32 @@ import sqlite3
 from sqlite3 import Error
 
 # Creates database connection, creates 'users' table if there is no existing 'users' table
-db_file = "RCMaster.db"
-con = None
-try:
-    con = sqlite3.connect(db_file)
-except Error as e:
-    print(e)
-if con:
-    cur = con.cursor()
-    print('ensure USERS table exists',flush=True)
-    cur.execute("CREATE TABLE IF NOT EXISTS users (user STRING PRIMARY KEY UNIQUE, points INTEGER UNIQUE, flair STRING);")
-    con.commit()
+DB_FILE = "../RCMaster.sql"
+__CONN = None # Module State? Not sure what this means. Seems to be a standard, though?
+
+def cursor():
+    global __CONN
+    if __CONN:
+        return __CONN
+
+    __CONN = sqlite3.connect(DB_FILE, isolation_level=None)  # auto-commit as we don't need transactions
+    
+    return __CONN
+
+def setup_db():
+    cursor().execute("CREATE TABLE IF NOT EXISTS users (user STRING PRIMARY KEY UNIQUE, points INTEGER UNIQUE, flair STRING);")
 
 def get_points(user):
-# Get's current Points by Connecting to the file and executing a command to return current value in "points". returns "None" even if you attempt to convert "None" to 0 explicitly. 
-    if user_check(user):
-        print('Returning current points value')
-        p = cur.execute("""SELECT points FROM users WHERE user = ?;""",(user,)).fetchone()
-        return int(p[0])
+    if row := cursor().execute("SELECT points from users where user = ?;", (user, )).fetchone():
+        return row[0]
         
-def get_flair(user):
-    # get's current flair and passes the value forward. mod_flair() has logic to handle None value. (there is no other reason to have this method called from anywhere else)
-    if user_check(user):
-        for row in cur.execute("""select flair from users where user = ?;""",(user,)):
-            if row[0] == None:
-                return "Riders Challenge Participant"
-            else:
-                return row[0]
-
 def add_point(user):
-    if user_check(user):       
-        try:
-            cur.execute("UPDATE users SET points = ? WHERE user = ?;", (get_points(user) + 1, user,))
-            con.commit()
-            print('point added. Current points: ' + str(cur.execute("""SELECT points FROM users WHERE user = ?;""",(user,)).fetchone()))
-        except Error as e:
-            print("""Something didn't work when adding a point for ?. Returning the Error message: ?""",(user,e,))
-            return(e)
-
-def user_check(user):
-    if cur.execute("select 1 from users where user = ?;", (user, )).fetchone():
-        print('User Exists: ' + str(cur.execute("""SELECT * FROM users WHERE user = ?;""",(user,)).fetchall()))
-        return True
+    if user_exists(user):
+        cursor().execute("UPDATE users SET points = ? WHERE user = ?;", (get_points(user) + 1,
+                                                                        user))
     else:
-        print('User did not exist. Creating now...')
-        cur.execute("""INSERT INTO users VALUES (?,?,?);""",(user,int(0),"Riders Challenge Participant",))
-        print("User Created: " + str(cur.execute("""SELECT * FROM users WHERE user = ?;""",(user,)).fetchall())) 
+        cursor().execute("INSERT INTO users (user, points) VALUES (?, ?);", (user, 0))
+
+def user_exists(user):
+    if cursor().execute("select 1 from users where user = ?;", (user, )).fetchone():
         return True
--- run.py
+++ run.py
@@ -2,7 +2,9 @@ import praw
 import functions.db as db
 import prawcore.exceptions as pc
 
-
+#################################################################
+###################### REDDIT INTERACTIONS ######################
+#################################################################
 
 #Establish a Reddit Session
 reddit = praw.Reddit()
@@ -11,93 +13,96 @@ sub = reddit.subreddit("riderschallengetest")
 def process_post(post):
     user = post.author.name
     db.add_point(user)
-    flair_update = str(db.get_points(user)) + ' | ' + db.get_flair(user)
-    print(flair_update)
+    if len(post.author_flair_text.split("|")) == 2: 
+        flair_update = str(db.get_points(user)) + ' | ' + post.author_flair_text
+    else:
+        flair_update = str(db.get_points(user) + "Riders Challenge Participant")
     return flair_update
 
 #check for formerly stickied post
 def get_sticky_id():
-    print("attempting to get stickies...")
     try:
         return sub.sticky(number=2).id
     except pc.NotFound as e:
         print(e)
         return False
-    except pc.Forbidden as f:
-        print(f)
-        return False
+
+def get_flair(post):
+    flair_text = ''
+    if post.author_flair_text != '':
+        flair_text = post.author_flair_text
+    return flair_text
 
 # Listening for Complete keyword in titles to reply to. 
-for post in reddit.subreddit("riderschallengetest").stream.submissions():
+# for post in reddit.subreddit("riderschallengetest").stream.submissions():
     
-    if post.saved:
-        continue
+#     if post.saved:
+#         continue
+
+#     has_keyword = any(k.lower() in post.title.lower() for k in ["complete"])
+#     not_self = post.author.name != reddit.user.me().name
     
-
-
-    has_keyword = any(k.lower() in post.title.lower() for k in ["complete"])
-    not_self = post.author.name != reddit.user.me.name()
-    if has_keyword and not_self:
-        post.save()
-        sticky_id = get_sticky_id()
-        print(sticky_id)
-        try:
-            sticky = reddit.submission(sticky_id)
-            print(type(sticky))
-            print(sticky)
-            sticky.mod.flair(text="",)    
-        except pc.Forbidden as f:
-            print(f)
-            continue
+#     if has_keyword and not_self:
+#         post.save()
+#         sticky_id = get_sticky_id()
+        
+#         sticky = reddit.submission(sticky_id)
+#         sticky.mod.flair(text="Completed",)    
             
-        to_post = process_post(post)
-        print(to_post)
-        reddit.subreddit("riderschallengetest").flair.set(post.author.name, text=to_post)
-        post.mod.sticky()
-        post.mod.flair(text="Current Challenge",)
+#         to_post = process_post(post)
+#         sub.flair.set(post.author.name, text=to_post)
+#         post.mod.sticky()
+#         post.mod.flair(text="Current Challenge",)
 
 ###################################################################
 ############ Implementation of multi-stream listening: ############
 ###################################################################
 
-# comment_stream = sub.stream.comments(pause_after=-1) # Special Case to be able to pass "None" to active listener
-# submission_stream = sub.stream.submissions(pause_after=-1) # Special Case to be able to pass "None" to active listener
-# while True:
+comment_stream = sub.stream.comments(pause_after=-1) # Special Case to be able to pass "None" to active listener
+submission_stream = sub.stream.submissions(pause_after=-1) # Special Case to be able to pass "None" to active listener
+while True:
 
 #___________________Active Listener for Comments___________________#
 
-#     for comment in comment_stream:
-#         if comment is None:
-#             break
-#         print(comment.author)
+    for comment in comment_stream:
+        if comment is None:
+            break
+        if comment.saved:
+            continue
+
+        if comment.body.find("!modflair"):
+            comment.save()
+            new_flair = comment.body[10:]
+            db.mod_flair(comment.body.author, new_flair)
+            print("Code to handle api interaction to be placed here eventually")
+        else:
+            continue
 
 #__________________Active Listener for Submissions__________________#
 
-#     for submission in submission_stream:
-#         if submission is None:
-#             break
-#         for post in reddit.subreddit("riderschallengetest").stream.submissions():
+    for submission in submission_stream:
+        if submission is None:
+            break
+        for post in reddit.subreddit("riderschallengetest").stream.submissions():
 
-        # if post.saved:
-        #     continue
+            if post.saved:
+                continue
 
-        # has_keyword = any(k.lower() in post.title.lower() for k in KEYWORDS)
-        # not_self = post.author.name != reddit.user.me.name()
-        # if has_keyword and not_self:
-        #     post.save()
-        #     sticky_id = get_sticky_id()
-        #     print(sticky_id)
-        #     try:
-        #         sticky = reddit.submission(sticky_id)
-        #         print(type(sticky))
-        #         print(sticky)
-        #         sticky.mod.flair(text="",)    
-        #     except pc.Forbidden as f:
-        #         print(f)
-        #         continue
+            has_keyword = any(k.lower() in post.title.lower() for k in ['complete'])                  # Checks if lower-case title has keyword "complete"
+            not_self = post.author.name != reddit.user.me.name()                                      # Checks if the bot is doing the posting
+            if has_keyword and not_self:                                                              # Makes sure that the following actions only ever fire if the bot didn't post it and the keyword was found
+                post.save()                                                                           # Saves the post to enable skipping existing posts later. 
+                sticky_id = get_sticky_id()                                                           # runs a method that grabs the Post ID of the current stickied challenge
+                print(sticky_id)                                                                      # Verifies we have a stickied ID in the variable  
+                try:                                              
+                    sticky = reddit.submission(sticky_id)                                             # Stores the post object of the current sticky
+                    print(type(sticky))                                                               # ensures object type "Object" (Debug, will delete eventually, or convert to log entry)
+                    sticky.mod.flair(text="",)                                                        # Un-sets any flair it has to remove the "Current Challenge" flair
+                except pc.Forbidden as f:
+                    print(f)                                                                          # Useless error handling that just prints the error, but tells the script to continue instead of exiting on an error.
+                continue                                                                              # Usually, this error was called when I was trying to use submission.mod.distinguish(), which apparently has issues. 
                 
-        #     to_post = process_post(post)
-        #     print(to_post)
-        #     reddit.subreddit("riderschallengetest").flair.set(post.author.name, text=to_post)
-        #     post.mod.sticky()
-        #     post.mod.flair(text="Current Challenge",)
+            to_post = process_post(post)                                                              # Runs a process on the post object of this for loop iteration to          
+            reddit.subreddit("riderschallengetest").flair.set(post.author.name, text=to_post)         # add points, 
+            post.mod.sticky()                                                                         # Sticky the post, 
+            post.mod.flair(text="Current Challenge",)                                                 # and add "Current Challenge" as flair to the pinned post. 
